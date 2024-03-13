094e7974846227b65c6273c54f40b6c8a44bcf67
Mahmut Fikret Gezer <64322071+FikretGezer@users.noreply.github.com>
2024-03-13 18:21:40 +0300

    Weather image and hamburger menu animation

--- WeatherAppInterface/frontend/scripts/main.js
+++ WeatherAppInterface/frontend/scripts/main.js
@@ -1,13 +1,16 @@
 const hamburgerMenuBtn = document.querySelector('.hamburger-menu img');
 const sideMenu = document.querySelector('.side-menu');
-const deneme = function()
+const appTitle = document.querySelector('.app-title');
+const weatherImage = document.querySelector('.weather-image');
+
+const startSideBarAnim = function()
 {
     hamburgerMenuBtn.style.opacity = "0";
     sideMenu.style.width = "70%";
     playAnimation('animated');
 }
 
-hamburgerMenuBtn.addEventListener('click', deneme);
+hamburgerMenuBtn.addEventListener('click', startSideBarAnim);
 
 document.addEventListener('click', event => {
     var isInside = document.querySelector('.side-menu').contains(event.target) ||
@@ -21,9 +24,30 @@ document.addEventListener('click', event => {
 });
 
 function playAnimation(animName) {
-    var box = sideMenu;
-    box.classList.add(animName);
-    setTimeout(function() {
-        box.classList.remove(animName);
-    }, 300); // Set the timeout to match the animation duration
+    sideMenu.classList.add(animName);
+    setTimeout(() => {
+        sideMenu.classList.remove(animName);
+    }, 300);
 }
+function remapValue(value, inputMin, inputMax, outputMin, outputMax) {
+    var normalizedValue = (value - inputMin) / (inputMax - inputMin);
+
+    var remappedValue = normalizedValue * (outputMax - outputMin) + outputMin;
+
+    return remappedValue;
+}
+const currentScreenWidth = window.innerWidth;
+document.addEventListener('mousemove', event => {
+    var mouseX = event.clientX;
+    var remapedValue = 0;
+    if(mouseX > currentScreenWidth / 2)
+    {
+        remapedValue = remapValue(mouseX, currentScreenWidth / 2, currentScreenWidth, 0, 65);
+        weatherImage.style.setProperty('--td-rotateY',`rotate3d(1, 1, 1, ${remapedValue}deg)`);
+    }
+    else
+    {
+        remapedValue = remapValue(mouseX, 0, currentScreenWidth / 2, -65, 0);
+        weatherImage.style.setProperty('--td-rotateY',`rotate3d(1, 1, 1, ${remapedValue}deg)`);
+    }
+});
